9e97d52bd57de66c4a398044394f388a4e577e25
Ayman Algamal <ayman01287985950@gmail.com>
2024-01-19 20:50:18 +0200

    Converting the QueueManager to a record as all of its fields are final.

--- backend/src/main/java/com/ayman/ProducerConsumer/models/QueueManager.java
+++ backend/src/main/java/com/ayman/ProducerConsumer/models/QueueManager.java
@@ -2,29 +2,7 @@ package com.ayman.ProducerConsumer.models;
 
 import java.util.Queue;
 
-public class QueueManager {
-
-    final int id;
-    String color;
-    Queue<Machine> listeners;
-
-    public QueueManager(int id, String color, Queue<Machine> listeners) {
-        this.id = id;
-        this.color = color;
-        this.listeners = listeners;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Queue<Machine> getListeners() {
-        return listeners;
-    }
+public record QueueManager(int id, String color, Queue<Machine> listeners) {
 
     public int getListenersCount() {
         return listeners.size();
